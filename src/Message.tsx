aab7d49b875bf7e6c9119e48ebe481b806f2c28b
Bishwas Bhatt <bishwasbhatt2@gmail.com>
2023-12-17 05:09:41 -0500

    Created a new app, and added ListGroup component

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message() {
+    // JSX: javaScript XML
+    const name = '';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    else
+        return <h1>Hello World!</h1>
+}
+
+export default Message;
